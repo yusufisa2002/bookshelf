325321fed9c71f94f0282f95f504892a2e073037
Yusuf <yusufisaabdullatif@gmail.com>
2023-03-25 06:35:33 +0700

    fix bugs host value

--- src/handler.js
+++ src/handler.js
@@ -166,7 +166,7 @@ const getBookByIdHandler = (request, h) => {
 const editBookByIdHandler = (request, h) => {
     const { bookId } = request.params;
     
-    const { name, year, author, summary, publisher, pageCount, readPage, finished, reading } = request.payload;
+    const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
 
     if (!name) {
         const response = h.response({
@@ -198,7 +198,6 @@ const editBookByIdHandler = (request, h) => {
             publisher,
             pageCount,
             readPage,
-            finished,
             reading,
             updatedAt,
         };
