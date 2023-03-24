75cd99d03513486fa62d26f96c6df9a1a24ea349
Yusuf <yusufisaabdullatif@gmail.com>
2023-03-25 05:25:20 +0700

    fix bugs host value

--- src/handler.js
+++ src/handler.js
@@ -171,45 +171,52 @@ const editBookByIdHandler = (request, h) => {
 
     const index = books.findIndex((buku) => buku.id === bookId);
 
-    if (!name) {
+    if (index !== -1) {
+        if (!name) {
+            const response = h.response({
+                status: 'fail',
+                message: 'Gagal memperbarui buku. Mohon isi nama buku',
+            });
+            response.code(400);
+            return response;
+        }
+
+        if (readPage > pageCount) {
+            const response = h.response({
+                status: 'fail',
+                message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+            });
+            response.code(400);
+            return response;
+        }
+
+        const finished = (pageCount === readPage);
+    
+        books[index] = {
+            ...books[index],
+            name,
+            year,
+            author,
+            summary,
+            publisher,
+            pageCount,
+            readPage,
+            reading,
+            updatedAt,
+        };
+
         const response = h.response({
-            status: 'fail',
-            message: 'Gagal memperbarui buku. Mohon isi nama buku',
+            status: 'success',
+            message: 'Buku berhasil diperbarui',
         });
-        response.code(400);
-        return response;
-    } else if (index === -1) {
-        const response = h.response({
-            status: 'fail',
-            message: 'Gagal memperbarui buku. Id tidak ditemukan',
-        });
-        response.code(404);
-        return response;
-    } else if (readPage > pageCount) {
-        const response = h.response({
-            status: 'fail',
-            message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
-        });
-        response.code(400);
+        response.code(200);
         return response;
     }
-    books[index] = {
-        ...books[index],
-        name,
-        year,
-        author,
-        summary,
-        publisher,
-        pageCount,
-        readPage,
-        reading,
-        updatedAt,
-    };
     const response = h.response({
-        status: 'success',
-        message: 'Buku berhasil diperbarui',
+        status: 'fail',
+        message: 'Gagal memperbarui buku. Id tidak ditemukan',
     });
-    response.code(200);
+    response.code(404);
     return response;
 };
 
