793aef96103cc9165b9a3e2b3c4eb8dece2323c5
Yusuf <yusufisaabdullatif@gmail.com>
2023-03-24 22:23:05 +0700

    fix bugs host value

--- src/handler.js
+++ src/handler.js
@@ -185,11 +185,10 @@ const editBookByIdHandler = (request, h) => {
         });
         response.code(404);
         return response;
-    }else if (readPage > pageCount) {
+    } else if (readPage > pageCount) {
         const response = h.response({
             status: 'fail',
-            message:
-            'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+            message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
         });
         response.code(400);
         return response;
